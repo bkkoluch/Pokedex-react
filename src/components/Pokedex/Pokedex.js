816642ec72835d421ae0ba209728be93a5b2377a
Bartek <bk.koluch@gmail.com>
2020-07-15 22:13:00 +0200

    Added the images for each mapped pokemon and some styling

--- src/components/Pokedex/Pokedex.js
+++ src/components/Pokedex/Pokedex.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import pokemonData from '../../assets/pokemon/pokemon.json';
+import styles from './Pokedex.module.css';
 import Pokemon from '../Pokemon/Pokemon';
 
 export default function Pokedex() {
 	return (
 		<div>
-			<ul>
+			<ul className={styles.pokedex}>
 				{console.log(pokemonData[0].identifier)}
 				{pokemonData.map((pokemonData, index) => (
 					<Pokemon
 						key={pokemonData.id}
 						name={pokemonData.identifier}
+						image={require('../../assets/sprites/pokemon/' +
+							`${index + 1}` +
+							'.png')}
 					/>
 				))}
 			</ul>
