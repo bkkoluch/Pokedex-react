c9d9af01f5ee7f85432f11e5565177ec7c11b1e4
Bartek <bk.koluch@gmail.com>
2020-07-16 21:11:42 +0200

    Added transfering correct props to detailedPokemon onClick

--- src/components/Pokemon/Pokemon.js
+++ src/components/Pokemon/Pokemon.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import styles from './Pokemon.module.css';
 import { Redirect } from 'react-router-dom';
-
-export default function Pokemon(props, { history }) {
+export default function Pokemon(props) {
 	const [redirect, setRedirect] = useState(false);
 
 	const handleRedirectToDetailedPokemon = () => {
@@ -10,7 +9,15 @@ export default function Pokemon(props, { history }) {
 	};
 
 	if (redirect) {
-		return <Redirect push to='/pokemon' />;
+		return (
+			<Redirect
+				to={{
+					pathname: '/pokemon',
+					name: props.name,
+					image: props.image,
+				}}
+			/>
+		);
 	}
 
 	return (
@@ -18,7 +25,7 @@ export default function Pokemon(props, { history }) {
 			className={styles.pokemon__container}
 			onClick={handleRedirectToDetailedPokemon}
 		>
-			<img src={props.image} />
+			<img src={props.image} alt={`${props.name} image`} />
 			<h5 className={styles.pokemon__name}>{props.name}</h5>
 		</div>
 	);
