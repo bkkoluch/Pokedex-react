1775613f4b586b2f460d5d2cfc32a120f8285640
Bartek <bk.koluch@gmail.com>
2020-07-16 20:04:12 +0200

    Added react router to transfer to pokemon onClick

--- src/components/Pokemon/Pokemon.js
+++ src/components/Pokemon/Pokemon.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Pokemon.module.css';
+import { Redirect } from 'react-router-dom';
 
-export default function Pokemon(props) {
+export default function Pokemon(props, { history }) {
+	const [redirect, setRedirect] = useState(false);
+
+	const handleRedirectToDetailedPokemon = () => {
+		setRedirect(true);
+	};
+
+	if (redirect) {
+		return <Redirect push to='/pokemon' />;
+	}
+
 	return (
-		<div className={styles.pokemon__container}>
+		<div
+			className={styles.pokemon__container}
+			onClick={handleRedirectToDetailedPokemon}
+		>
 			<img src={props.image} />
 			<h5 className={styles.pokemon__name}>{props.name}</h5>
 		</div>
