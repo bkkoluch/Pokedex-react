b3d0bbfa0edf1f36ee5e765cfcbe9915b00840bf
Bartek <bk.koluch@gmail.com>
2020-08-16 22:47:32 +0200

    Added the input handler

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import { ROUTES } from 'utils/utils';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+
+import { ROUTES } from 'utils/utils';
 import styles from './Header.module.css';
 
 interface HeaderProps {
 	detailed?: boolean;
 }
+
 const Header: React.FC<HeaderProps> = ({ detailed }) => {
+	const [input, setInput] = useState('');
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setInput(e.target.value);
+	};
+
 	return (
 		<div className={styles.header__container}>
 			<Link className={styles.header__link} to={ROUTES.ROOT}>
@@ -20,6 +28,8 @@ const Header: React.FC<HeaderProps> = ({ detailed }) => {
 					className={styles.header__input}
 					type='text'
 					placeholder='Search for a pokemon'
+					value={input}
+					onChange={handleChange}
 				></input>
 			) : (
 				''
